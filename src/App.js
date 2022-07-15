37272bc9863eb75a7f43cd600d7431bb59f2037f
Anthony Aromin <menekoiskawaii@gmail.com>
2022-07-15 19:34:31 +0800

    Add empty CV component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import CV from './components/CV';
 
 function App() {
   return (
     <div className="App">
       <Header />
-
+      <div id='cv-document-container'>
+        <CV />
+      </div>
       <Footer />
     </div>
   );
---
+++ src/components/CV/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+class CV extends React.Component {
+  render() {
+    return (
+      <div id='cv-document'>
+
+      </div>
+    );
+  }
+}
+
+export default CV;
