32808ef599a496c2eea80064ade4ea5caa408481
Anthony Aromin <menekoiskawaii@gmail.com>
2022-07-15 18:23:03 +0800

    Add Header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.css';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>CV Editor</h1>
+        <div>A simple CV maker and editor in ReactJS.</div>
+      </header>
+    );
+  }
+}
+
+export default Header;
