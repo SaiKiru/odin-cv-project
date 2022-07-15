9f38995161571b3e986b6851e80455f7ee9b0b73
Anthony Aromin <menekoiskawaii@gmail.com>
2022-07-15 18:23:39 +0800

    Add Footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './index.css';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        Made by <a
+          href='https://github.com/SaiKiru'
+          target='_blank'
+          rel='noreferrer'
+        >saikiru</a>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
